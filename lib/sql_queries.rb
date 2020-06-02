547bef5a37ef67b65264cd9829024758e1800c94
Steve Anderson <prototype909@msn.com>
2020-06-02 19:17:26 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -16,7 +16,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  "Select users.name, users.age, SUM (pledges.amount)
+  "SELECT users.name, users.age, SUM (pledges.amount)
    FROM users
    INNER JOIN pledges
    ON users.id = pledges.user_id
@@ -25,11 +25,16 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "Write your SQL query Here"
+  "SELECT title, SUM (pledges.amount) - projects.funding_goal AS amount_over_goal
+   FROM projects
+   INNER JOIN pledges
+   ON projects.id = pledges.project_id
+   GROUP BY project_id
+   HAVING SUM (pledges.amount) >= project.funding_goal"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
-  "Write your SQL query Here"
+  "SELECT users.name, users.amount SUM (pledges)"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
